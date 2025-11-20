e0e6c62f2d7680e543baf99993f7de8c606e6fe2
maxmfarias <estudamax4@gmail.com>
2025-11-19 22:18:39 -0300

    Delete sistema-de-avaliacao.py

--- sistema-de-avaliacao.py
+++
@@ -1,9 +0,0 @@
-nota1 = float(input("Digite a primeira nota:"))
-nota2 = float(input("Digite a segunda nota:"))
-media = (nota1 + nota2) // 2
-
-if media >= 7.00:
-    print("Resultado: Aprovado")
-
-else:
-    print("Resultado: Reprovado")
